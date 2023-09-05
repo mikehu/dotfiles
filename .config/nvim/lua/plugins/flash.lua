d53182922603f3b4c7530c22bfaa4ec2c9993eaf
Mike Hu <m@mikehu.com>
2023-09-04 21:50:41 -0700

    fix lua bug and add fonts

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -12,46 +12,44 @@ return {
 	},
 	keys = {
 		{
-			{
-				"s",
-				mode = { "n", "x" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "x" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			-- {
-			-- 	"r",
-			-- 	mode = "o",
-			-- 	function()
-			-- 		flash.remote()
-			-- 	end,
-			-- 	desc = "Remote Flash",
-			-- },
-			-- {
-			-- 	"R",
-			-- 	mode = { "o", "x" },
-			-- 	function()
-			-- 		flash.treesitter_search()
-			-- 	end,
-			-- 	desc = "Treesitter Search",
-			-- },
-			-- {
-			-- 	"<c-s>",
-			-- 	mode = { "c" },
-			-- 	function()
-			-- 		flash.toggle()
-			-- 	end,
-			-- 	desc = "Toggle Flash Search",
-			-- },
+			"s",
+			mode = { "n", "x" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
 		},
+		{
+			"S",
+			mode = { "n", "x" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		-- {
+		-- 	"r",
+		-- 	mode = "o",
+		-- 	function()
+		-- 		flash.remote()
+		-- 	end,
+		-- 	desc = "Remote Flash",
+		-- },
+		-- {
+		-- 	"R",
+		-- 	mode = { "o", "x" },
+		-- 	function()
+		-- 		flash.treesitter_search()
+		-- 	end,
+		-- 	desc = "Treesitter Search",
+		-- },
+		-- {
+		-- 	"<c-s>",
+		-- 	mode = { "c" },
+		-- 	function()
+		-- 		flash.toggle()
+		-- 	end,
+		-- 	desc = "Toggle Flash Search",
+		-- },
 	},
 }
