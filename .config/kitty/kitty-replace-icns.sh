29ce4311a96c0950603aa4f93b06bf02f0f178c6
Mike Hu <m@mikehu.com>
2024-02-23 22:17:56 -0800

    kitty replace icon shell script

---
+++ .config/kitty/kitty-replace-icns.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' ~/.config/kitty/kitty-dark.icns
+rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
