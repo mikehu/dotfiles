3f20bce68f23a909d01af6d664f140f283457dd3
Mike Hu <m@mikehu.com>
2024-11-19 15:39:51 -0800

    change where lazy loading occurs for lsp/conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,6 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPost", "BufNewFile" },
+	event = { "BufReadPre", "BufNewFile" },
 	opts = {},
 	config = function()
 		local conform = require("conform")
