7005368ee79e39804a90e98349a88650707f0447
Mike Hu <m@mikehu.com>
2025-01-22 09:50:59 -0800

    update to new supported colorizer plugin

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,11 +1,13 @@
 return {
-	"NvChad/nvim-colorizer.lua",
+	"catgoose/nvim-colorizer.lua",
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("colorizer").setup({
-			user_default_options = {
-				tailwind = true,
+	opts = {
+		user_default_options = {
+			rgb_fn = true,
+			tailwind = "both",
+			tailwind_opts = {
+				update_names = true,
 			},
-		})
-	end,
+		},
+	},
 }
