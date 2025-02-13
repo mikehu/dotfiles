90a3b3b53513b82cf5c84619619ac83164c7aee7
Mike Hu <m@mikehu.com>
2025-02-13 10:37:38 -0800

    fix telescope not being able to send to quicklist

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -34,7 +34,6 @@ return {
 						i = {
 							["<C-j>"] = actions.move_selection_next, -- move to next result
 							["<C-k>"] = actions.move_selection_previous, -- move to prev result
-							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
 						},
 						n = {
 							["q"] = actions.close,
