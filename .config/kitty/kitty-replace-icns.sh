70c774ffcf797ebd30e97297d7788b1ce338b91e
Mike Hu <m@mikehu.com>
2025-02-13 10:37:53 -0800

    updated kitty icon and aerospace config

--- .config/kitty/kitty-replace-icns.sh
+++ .config/kitty/kitty-replace-icns.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' ~/.config/kitty/kitty-dark.icns
+kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' ~/.config/kitty/kitty.app.png
 rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
