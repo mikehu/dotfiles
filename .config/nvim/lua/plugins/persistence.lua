392f3ea6dc509453eb1940bc1916dfa9a3d94c55
Mike Hu <m@mikehu.com>
2025-02-28 10:58:22 -0800

    fix persistence causing startup issues

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -4,17 +4,16 @@ return {
 		local persist = require("persistence")
 		persist.setup({ need = 2 })
 
-		local group = vim.api.nvim_create_augroup("PersistEvents", { clear = true })
 		vim.api.nvim_create_autocmd("VimEnter", {
-			group = group,
+			group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
 			callback = function()
 				if vim.fn.argc(-1) == 0 then
-					vim.api.nvim_exec_autocmds("User", { pattern = "PersistEnter" })
+					persist.load()
 				else
 					persist.stop()
 				end
 			end,
-			once = true,
+			nested = true,
 		})
 	end,
 }
