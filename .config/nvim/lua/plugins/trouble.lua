76a611bb3cd4c915f72c332b08c1c4ef8a084c33
Mike Hu <m@mikehu.com>
2025-03-03 14:46:09 -0800

    remove trouble diagnostics for current buffer keymapping

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -9,12 +9,6 @@ return {
 		local wk = require("which-key")
 		wk.add({
 			{ "<leader>tt", [[<cmd>Trouble diagnostics toggle<cr>]], desc = "Diagnostics", icon = "🚦" },
-			{
-				"<leader>tb",
-				[[<cmd>Trouble diagnostics toggle filter.buf=0<cr>]],
-				desc = "Buffer diagnostics",
-				icon = "🚦",
-			},
 			{ "<leader>tq", [[<cmd>Trouble qflist toggle<cr>]], desc = "Quickfix list", icon = "🚦" },
 			{ "<leader>tl", [[<cmd>Trouble loclist toggle<cr>]], desc = "Location list", icon = "🚦" },
 		})
