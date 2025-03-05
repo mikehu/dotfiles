946316e76ebdd0755d1b349902ad667a9e1543bd
Mike Hu <m@mikehu.com>
2025-03-05 15:52:48 -0800

    added encoding/decoding tools to toolbox

--- .config/nvim/lua/plugins/toolbox.lua
+++ .config/nvim/lua/plugins/toolbox.lua
@@ -1,9 +1,31 @@
-local function cmd_on_selection_or_line(cmd)
-	local mode = vim.fn.mode()
-	if mode == "v" or mode == "V" or mode == "\22" then
-		cmd = "'<,'>" .. cmd
+local TextType = {
+	BUFFER = "%",
+	LINE = ".",
+}
+
+local function is_selecting()
+	local start_pos = vim.fn.getpos("'<")
+	local end_pos = vim.fn.getpos("'>")
+
+	local line_start, col_start = start_pos[2], start_pos[3]
+	local line_end, col_end = end_pos[2], end_pos[3]
+
+	return line_start ~= line_end or col_start ~= col_end
+end
+
+local function cmd_on_text(cmd, external, type)
+	type = type or TextType.LINE
+	if external == nil then
+		external = false
 	end
-	return cmd
+	if external then
+		cmd = "!" .. cmd
+	end
+	if is_selecting() then
+		return "'<,'>" .. cmd
+	else
+		return type .. cmd
+	end
 end
 
 local function safe_vim_cmd(cmd)
@@ -26,6 +48,7 @@ return {
 	event = "VeryLazy",
 	keys = {
 		{
+			mode = { "n", "x" },
 			"<leader>x",
 			function()
 				require("toolbox").show_picker()
@@ -38,7 +61,31 @@ return {
 			{
 				name = "Unescape slashes",
 				execute = function()
-					safe_vim_cmd(cmd_on_selection_or_line("s/\\\\\\//\\//g"))
+					safe_vim_cmd(cmd_on_text("s/\\\\\\//\\//g"))
+				end,
+			},
+			{
+				name = "Base64 encode",
+				execute = function()
+					safe_vim_cmd(cmd_on_text("base64", true))
+				end,
+			},
+			{
+				name = "Base64 decode",
+				execute = function()
+					safe_vim_cmd(cmd_on_text("base64 --decode", true))
+				end,
+			},
+			{
+				name = "URL encode",
+				execute = function()
+					safe_vim_cmd(cmd_on_text("perl -MURI::Escape -ne 'print uri_escape($_)'", true))
+				end,
+			},
+			{
+				name = "URL decode",
+				execute = function()
+					safe_vim_cmd(cmd_on_text("perl -MURI::Escape -ne 'print uri_unescape($_)' | tr -d '\\n'", true))
 				end,
 			},
 		},
