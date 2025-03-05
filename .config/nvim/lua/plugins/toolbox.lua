908d5869d92dd9954f624abdb89685d7682f229f
Mike Hu <m@mikehu.com>
2025-03-05 11:59:07 -0800

    remove telescope dependency for toolbox

--- .config/nvim/lua/plugins/toolbox.lua
+++ .config/nvim/lua/plugins/toolbox.lua
@@ -1,17 +1,46 @@
+local function cmd_on_selection_or_line(cmd)
+	local mode = vim.fn.mode()
+	if mode == "v" or mode == "V" or mode == "\22" then
+		cmd = "'<,'>" .. cmd
+	end
+	return cmd
+end
+
+local function safe_vim_cmd(cmd)
+	local success, err = pcall(function()
+		vim.cmd(cmd)
+	end)
+
+	-- Suppress E486 (Pattern not found) but allow other errors to be printed
+	if not success and err:match("E486") then
+		return false -- Indicate that nothing was replaced
+	elseif not success then
+		vim.api.nvim_err_writeln("Error: " .. err)
+	end
+
+	return success
+end
+
 return {
 	"DanWlker/toolbox.nvim",
 	event = "VeryLazy",
-	dependencies = { "nvim-telescope/telescope.nvim" },
 	keys = {
 		{
-			"<leader>fx",
+			"<leader>x",
 			function()
 				require("toolbox").show_picker()
 			end,
-			desc = "Scripts toolbox",
+			desc = "Toolbox",
 		},
 	},
 	opts = {
-		commands = {},
+		commands = {
+			{
+				name = "Unescape slashes",
+				execute = function()
+					safe_vim_cmd(cmd_on_selection_or_line("s/\\\\\\//\\//g"))
+				end,
+			},
+		},
 	},
 }
