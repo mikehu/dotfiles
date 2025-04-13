c743c076f8bdd19f8792597d35b2f2e5eb518469
Mike Hu <m@mikehu.com>
2025-04-12 18:23:37 -0700

    allow dynamically "unloading" of plugins in lazy when using a `_` prefix

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -12,9 +12,23 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	{ import = "plugins" },
-}, {
+-- Get plugin files
+local function get_plugins()
+	local plugins = {}
+	local plugin_path = vim.fn.stdpath("config") .. "/lua/plugins"
+	local files = vim.fn.glob(plugin_path .. "/*.lua", false, true)
+
+	for _, file in ipairs(files) do
+		local filename = vim.fn.fnamemodify(file, ":t:r")
+		if not filename:match("^_") then
+			table.insert(plugins, require("plugins." .. filename))
+		end
+	end
+
+	return plugins
+end
+
+require("lazy").setup(get_plugins(), {
 	install = {
 		missing = true,
 	},
