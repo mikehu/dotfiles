fbe386dcb6591298b9c0accf60e7d3a3784248da
Mike Hu <m@mikehu.com>
2025-05-13 13:03:53 -0700

    add codecompanion-history extension

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -33,6 +33,10 @@ return {
 	-- 	end,
 	-- },
 	{
+		"ravitemer/codecompanion-history.nvim",
+		event = "VeryLazy",
+	},
+	{
 		"ravitemer/mcphub.nvim",
 		event = "VeryLazy",
 		dependencies = {
@@ -56,6 +60,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 			"j-hui/fidget.nvim",
+			"ravitemer/codecompanion-history.nvim",
 			"ravitemer/mcphub.nvim",
 		},
 		init = function()
@@ -75,6 +80,30 @@ return {
 					},
 				},
 				extensions = {
+					history = {
+						opts = {
+							-- Keymap to open history from chat buffer (default: gh)
+							keymap = "gh",
+							-- Automatically generate titles for new chats
+							auto_generate_title = true,
+							-- On exiting and entering neovim, loads the last chat on opening chat
+							continue_last_chat = true,
+							-- When chat is cleared with `gx` delete the chat from history
+							delete_on_clearing_chat = false,
+							-- Picker interface ("telescope", "snacks" or "default")
+							picker = "snacks",
+							-- Enable detailed logging for history extension
+							enable_logging = false,
+							-- Directory path to save the chats
+							dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+							-- Save all chats by default
+							auto_save = true,
+							-- Keymap to save the current chat manually
+							save_chat_keymap = "gw",
+							-- Number of days after which chats are automatically deleted (0 to disable)
+							expiration_days = 7,
+						},
+					},
 					mcphub = {
 						callback = "mcphub.extensions.codecompanion",
 						opts = {
