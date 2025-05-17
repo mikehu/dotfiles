c36ba4bc3d73324187c2eead4b6f8494052f1f3d
Mike Hu <m@mikehu.com>
2025-05-16 22:44:28 -0700

    better feedback for clone worktree script

--- .config/scripts/git-clone-bare-for-worktrees.sh
+++ .config/scripts/git-clone-bare-for-worktrees.sh
@@ -12,6 +12,7 @@ url=$1
 basename=$(basename "$url")
 name=${2:-$(echo "$basename" | sed 's/\.git$//')}
 
+echo "Setting up worktree-ready repository in directory: $name"
 mkdir "$name"
 cd "$name"
 
@@ -24,6 +25,7 @@ cd "$name"
 # new-awesome-feature
 # hotfix-bug-12
 # ...
+echo "Cloning repository into $name/.bare (bare repository for worktree management)..."
 git clone --bare "$url" .bare
 echo "gitdir: ./.bare" > .git
 
@@ -32,3 +34,5 @@ git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 
 # Gets all branches from origin
 git fetch origin
+
+echo "Repository successfully set up in $name. You can now create worktrees with 'git worktree add'."
