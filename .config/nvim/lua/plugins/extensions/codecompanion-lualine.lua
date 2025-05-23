b863b2da62a3f47032d9756e005b847a4c85def1
Mike Hu <m@mikehu.com>
2025-05-22 22:54:14 -0700

    even more awesome lualine for codecompanion

--- .config/nvim/lua/plugins/extensions/codecompanion-lualine.lua
+++ .config/nvim/lua/plugins/extensions/codecompanion-lualine.lua
@@ -1,7 +1,8 @@
-local Spinner = require("lualine.component"):extend()
+local Component = require("lualine.component"):extend()
 
-Spinner.processing = false
-Spinner.spinner_index = 1
+Component.processing = false
+Component.spinner_index = 1
+Component.model = nil
 
 local spinner_symbols = {
 	"⠋",
@@ -18,8 +19,8 @@ local spinner_symbols = {
 local spinner_symbols_len = 10
 
 -- Initializer
-function Spinner:init(options)
-	Spinner.super.init(self, options)
+function Component:init(options)
+	Component.super.init(self, options)
 
 	local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
 
@@ -34,10 +35,20 @@ function Spinner:init(options)
 			end
 		end,
 	})
+
+	vim.api.nvim_create_autocmd({ "User" }, {
+		pattern = "CodeCompanionChatModel",
+		group = group,
+		callback = function(event)
+			if event.data and event.data.model then
+				Component.model = event.data.model
+			end
+		end,
+	})
 end
 
 -- Function that runs every time statusline is updated
-function Spinner:update_status()
+function Component:update_status()
 	if self.processing then
 		self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
 		return spinner_symbols[self.spinner_index]
@@ -46,14 +57,22 @@ function Spinner:update_status()
 	end
 end
 
+local function codecompanion_current_model()
+	if Component.model then
+		return "  " .. Component.model
+	end
+	return "  config not ready!"
+end
+
 local function codecompanion_modifiable_status()
 	local current_bufnr = vim.api.nvim_get_current_buf()
 	if not vim.api.nvim_get_option_value("modifiable", { buf = current_bufnr }) then
-		return "✨ Working on an answer..."
+		return "⏳ Working on an answer..."
 	else
-		return "✏️ Awaiting input..." -- Or perhaps '✏️' to indicate editable
+		return "✏️ Awaiting input..."
 	end
 end
+
 local Extension = {
 	filetypes = { "codecompanion" },
 	sections = {
@@ -61,6 +80,11 @@ local Extension = {
 			{
 				"filename",
 				path = 1,
+				separator = "⟩",
+			},
+			{
+				codecompanion_current_model,
+				separator = "⟩",
 			},
 			codecompanion_modifiable_status,
 		},
@@ -68,6 +92,6 @@ local Extension = {
 }
 
 return {
-	component = Spinner,
+	component = Component,
 	extension = Extension,
 }
