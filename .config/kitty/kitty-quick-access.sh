d09d16e489fddc20b4c21e107d83b1106c187509
Mike Hu <m@mikehu.com>
2025-06-19 16:49:15 -0700

    quick access script for raycast

---
+++ .config/kitty/kitty-quick-access.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Kitty Quick Access
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🖥️
+# @raycast.packageName Terminal
+
+kitten quick-access-terminal
