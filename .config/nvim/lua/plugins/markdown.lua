8b911f42e89eb66faf7fe75d9ce32c765a36e0ec
Mike Hu <m@mikehu.com>
2025-07-20 22:56:23 -0700

    markdown specific mapping for toggling checkboxes

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -17,6 +17,11 @@ return {
 				web = { pattern = "^http[s]?://", icon = " ", highlight = "RenderMarkdownLink" },
 			},
 		},
+		checkbox = {
+			checked = {
+				scope_highlight = "@comment",
+			},
+		},
 	},
 	config = function(_, opts)
 		require("render-markdown").setup(opts)
@@ -27,6 +32,32 @@ return {
 			callback = function()
 				vim.opt_local.wrap = true
 				vim.opt_local.linebreak = true
+				vim.keymap.set("n", "<leader>x", function()
+					local line = vim.api.nvim_get_current_line()
+					local new_line
+
+					-- Check if line contains unchecked checkbox and toggle to checked
+					if line:match("%[%s*%]") then
+						new_line = line:gsub("%[%s*%]", "[x]", 1)
+					-- Check if line contains checked checkbox and toggle to unchecked
+					elseif line:match("%[x%]") or line:match("%[X%]") then
+						new_line = line:gsub("%[[xX]%]", "[ ]", 1)
+					else
+						-- If no checkbox found, add one at the beginning of list item
+						-- Handle both bullet points and numbered lists
+						if line:match("^%s*[-*+]%s") then
+							-- Bullet point: "- item" -> "- [ ] item"
+							new_line = line:gsub("^(%s*[-*+]%s)", "%1[ ] ")
+						elseif line:match("^%s*%d+%.%s") then
+							-- Numbered list: "1. item" -> "1. [ ] item"
+							new_line = line:gsub("^(%s*%d+%.%s)", "%1[ ] ")
+						else
+							return -- Do nothing if not a list item
+						end
+					end
+
+					vim.api.nvim_set_current_line(new_line)
+				end, { buffer = true, desc = "Toggle markdown checkbox" })
 			end,
 		})
 	end,
