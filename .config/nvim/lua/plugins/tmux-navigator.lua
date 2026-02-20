981b153c7136ffc49b141334b1f074bd095d7268
Mike Hu <m@mikehu.com>
2026-02-20 08:47:41 -0800

    fix tmux navigation to account for neovim floats!

--- .config/nvim/lua/plugins/tmux-navigator.lua
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -7,9 +7,60 @@ return {
 			disable_when_zoomed = true, -- defaults to false
 		})
 
-		vim.keymap.set({ "n", "i", "t" }, "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-		vim.keymap.set({ "n", "i" }, "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-		vim.keymap.set({ "n", "i" }, "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-		vim.keymap.set({ "n", "i", "t" }, "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+		-- Floating filetypes that should be treated as a navigable layer.
+		-- When one of these is visible, it becomes the only thing C-h/j/k/l can reach.
+		local navigable_float_fts = {
+			difft = true,
+			lazy = true,
+			snacks_terminal = true,
+			snacks_notif_history = true,
+		}
+
+		local function find_navigable_float()
+			for _, win in ipairs(vim.api.nvim_list_wins()) do
+				local cfg = vim.api.nvim_win_get_config(win)
+				if cfg.relative and cfg.relative ~= "" and cfg.focusable ~= false then
+					local ft = vim.bo[vim.api.nvim_win_get_buf(win)].filetype
+					if navigable_float_fts[ft] then
+						return win
+					end
+				end
+			end
+			return nil
+		end
+
+		local function in_navigable_float()
+			local cfg = vim.api.nvim_win_get_config(0)
+			if cfg.relative and cfg.relative ~= "" then
+				local ft = vim.bo.filetype
+				return navigable_float_fts[ft]
+			end
+			return false
+		end
+
+		local function navigate_with_float_awareness(nav_fn, direction)
+			return function()
+				if in_navigable_float() then
+					-- Already in a navigable float: pass directly to tmux
+					if vim.env.TMUX then
+						require("nvim-tmux-navigation.tmux_util").tmux_change_pane(direction)
+					end
+				else
+					local float_win = find_navigable_float()
+					if float_win then
+						-- A navigable float exists: jump into it
+						vim.api.nvim_set_current_win(float_win)
+					else
+						-- No floats: normal navigation
+						nav_fn()
+					end
+				end
+			end
+		end
+
+		vim.keymap.set({ "n", "i", "t" }, "<C-h>", navigate_with_float_awareness(nvim_tmux_nav.NvimTmuxNavigateLeft, "h"))
+		vim.keymap.set({ "n", "i" }, "<C-j>", navigate_with_float_awareness(nvim_tmux_nav.NvimTmuxNavigateDown, "j"))
+		vim.keymap.set({ "n", "i" }, "<C-k>", navigate_with_float_awareness(nvim_tmux_nav.NvimTmuxNavigateUp, "k"))
+		vim.keymap.set({ "n", "i", "t" }, "<C-l>", navigate_with_float_awareness(nvim_tmux_nav.NvimTmuxNavigateRight, "l"))
 	end,
 }
