dd460a4eca0cbf12b3f198d844ffbe85af74e9c1
Mike Hu <m@mikehu.com>
2026-03-01 17:10:38 -0800

    update colorizer options

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -2,11 +2,10 @@ return {
 	"catgoose/nvim-colorizer.lua",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
-		user_default_options = {
-			rgb_fn = true,
-			tailwind = "both",
-			tailwind_opts = {
-				update_names = true,
+		options = {
+			parsers = {
+				rgb = { enable = true },
+				tailwind = { enable = true, lsp = true, update_names = true },
 			},
 		},
 	},
